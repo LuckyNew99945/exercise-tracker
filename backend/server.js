4053c60966b7bd58ccf94872961ab02ede477a93
LuckyNew99945 <luckynew99945@gmail.com>
2020-08-26 22:24:54 +0700

    inialize and make backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.log(err));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('mongodb ok');
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter); //load exercise.js in routers
+
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
+});
